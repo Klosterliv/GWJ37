f977407e1cc3a21f7bd94af7c3e452a7f6a6ba9c
ForrestRamsay <43358130+ForrestRamsay@users.noreply.github.com>
2021-09-12 13:14:11 -0500

    it kinda works now

--- Nanobot.gd
+++ Nanobot.gd
@@ -1,1 +1,14 @@
-extends Node2D
+extends KinematicBody2D
+
+export var speed = 50
+
+func _physics_process(delta):
+	move_and_slide(get_global_mouse_position())
+
+func _look_at_mouse():
+	look_at(get_global_mouse_position())
+	rotation_degrees = rotation_degrees + 90
+
+func _move_to_mouse():
+	var direction = get_global_mouse_position() - position
+	move_and_slide(direction)
