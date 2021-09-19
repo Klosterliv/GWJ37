25bc43b82e1e4fc385c62c0a3014390db208223d
ForrestRamsay <43358130+ForrestRamsay@users.noreply.github.com>
2021-09-19 14:04:44 -0500

    small changes

--- Scripts/WorldScript.gd
+++ Scripts/WorldScript.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var Game_Title = "Wonder-Drug"
+var Game_Title = "NanoBiotic"
 
 func _process(_delta):
 	OS.set_window_title(Game_Title + " | fps: " + str(Engine.get_frames_per_second()))
